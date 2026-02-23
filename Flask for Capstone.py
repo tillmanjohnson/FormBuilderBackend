4e8ad7ce17b02bb905024963302bb322b045c95d
Timmy Johnson <timmyjohnson@Timmys-MacBook-Air.local>
2026-02-23 12:11:08 -0500

    Initial commit

---
+++ Flask for Capstone.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from pymongo import MongoClient
+import certifi
+import os
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+CORS(app)
+
+load_dotenv()
+mongo_connection = os.environ.get("mongo_connection")
+client = MongoClient(mongo_connection,
+                     tlsCAFile=certifi.where()
+                     )
+
+db = client["FormBuilderDB"]
+messages = db["messages"]
+
+@app.route("/")
+def home():
+    return "API running"
+
+@app.route("/submit", methods=["POST"])
+def submit_text():
+    data = request.json
+
+    if not data or "text" not in data:
+        return jsonify({"error": "No text provided"}), 400
+
+
+    messages.insert_one({"text": data["text"]})
+
+    return jsonify({"message": "Saved successfully!"})
+
+@app.route("/test-db")
+def test_db():
+    try:
+        client.admin.command('ping')
+        return jsonify({"status": "Connected to MongoDB"})
+    except Exception as e:
+        return jsonify({"status": "Connection failed", "error": str(e)})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
